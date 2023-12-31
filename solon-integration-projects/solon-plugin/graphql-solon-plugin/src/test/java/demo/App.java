2c7be4a5d03137acfd58737120eb6dafe55d3162
fuzi1996 <imfuzi@163.com>
2023-12-31 15:25:01 +0800

    chore(graphql):不直接引用smarthttp,改为引用solon.net

--- solon-integration-projects/solon-plugin/graphql-solon-plugin/src/test/java/demo/App.java
+++ solon-integration-projects/solon-plugin/graphql-solon-plugin/src/test/java/demo/App.java
@@ -12,7 +12,7 @@ public class App {
 
     public static void main(String[] args) {
         Solon.start(App.class, args, app -> {
-            app.enableWebSocket(true);
+            app.enableWebSocket(false);
         });
     }
 }
