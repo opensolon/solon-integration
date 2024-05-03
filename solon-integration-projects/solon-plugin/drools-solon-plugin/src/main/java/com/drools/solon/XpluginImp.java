9900172039137c8dac7f44e5f260f8b4564e0b6b
noear <noear@live.cn>
2024-05-03 21:21:47 +0800

    优化 drools 适配

--- solon-integration-projects/solon-plugin/drools-solon-plugin/src/main/java/com/drools/solon/XpluginImp.java
+++ solon-integration-projects/solon-plugin/drools-solon-plugin/src/main/java/com/drools/solon/XpluginImp.java
@@ -1,7 +1,6 @@
 package com.drools.solon;
 
 import org.noear.solon.core.AppContext;
-import org.noear.solon.core.LifecycleIndex;
 import org.noear.solon.core.Plugin;
 
 import com.drools.solon.config.DroolsAutoConfiguration;
@@ -9,8 +8,6 @@ import com.drools.solon.config.DroolsAutoConfiguration;
 public class XpluginImp implements Plugin {
     @Override
     public void start(AppContext context) throws Throwable {
-    	context.lifecycle(LifecycleIndex.PLUGIN_BEAN_USES, () -> {
-    		context.beanMake(DroolsAutoConfiguration.class);
-    	});
+        context.beanMake(DroolsAutoConfiguration.class);
     }
 }
