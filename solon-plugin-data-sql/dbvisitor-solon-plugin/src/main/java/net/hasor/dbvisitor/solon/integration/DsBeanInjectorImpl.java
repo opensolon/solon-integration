39de630e6ee9daf358873eb77d03a7a8f6a68831
noear <noear@live.cn>
2024-09-09 10:27:13 +0800

    dbvisitor-solon-plugin 改为 Ds 注入方式

---
+++ solon-plugin-data-sql/dbvisitor-solon-plugin/src/main/java/net/hasor/dbvisitor/solon/integration/DsBeanInjectorImpl.java
@@ -0,0 +1,85 @@
+package net.hasor.dbvisitor.solon.integration;
+
+import net.hasor.dbvisitor.dal.mapper.BaseMapper;
+import net.hasor.dbvisitor.dal.repository.DalRegistry;
+import net.hasor.dbvisitor.dal.session.DalSession;
+import net.hasor.dbvisitor.jdbc.core.JdbcTemplate;
+import net.hasor.dbvisitor.lambda.LambdaTemplate;
+import org.noear.solon.core.BeanInjector;
+import org.noear.solon.core.BeanWrap;
+import org.noear.solon.core.VarHolder;
+import org.noear.solon.data.annotation.Ds;
+import org.noear.solon.data.datasource.DsUtils;
+
+import javax.sql.DataSource;
+
+/**
+ * @author noear
+ * @since 2.9
+ */
+public class DsBeanInjectorImpl implements BeanInjector<Ds> {
+    private final DalRegistry dalRegistry;
+    public DsBeanInjectorImpl(DalRegistry dalRegistry) {
+        this.dalRegistry = dalRegistry;
+    }
+
+    @Override
+    public void doInject(VarHolder vh, Ds anno) {
+        DsUtils.observeDs(vh.context(), anno.value(), (dsWrap) -> {
+            inject0(vh, dsWrap);
+        });
+    }
+
+    private void inject0(VarHolder varH, BeanWrap dsBw) {
+        try {
+            inject1(varH, dsBw);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void inject1(VarHolder varH, BeanWrap dsBw) throws Exception {
+        DataSource ds = dsBw.get();
+        Class<?> clz = varH.getType();
+
+        //@Db("db1") LambdaTemplate ; //顺序别乱变
+        if (LambdaTemplate.class.isAssignableFrom(varH.getType())) {
+            LambdaTemplate accessor = new LambdaTemplate(new SolonManagedDynamicConnection(ds));
+
+            varH.setValue(accessor);
+            return;
+        }
+
+        //@Db("db1") JdbcTemplate ;
+        if (JdbcTemplate.class.isAssignableFrom(varH.getType())) {
+            JdbcTemplate accessor = new JdbcTemplate(new SolonManagedDynamicConnection(ds));
+
+            varH.setValue(accessor);
+            return;
+        }
+
+        //@Db("db1") DalSession ;
+        if (DalSession.class.isAssignableFrom(varH.getType())) {
+            DalSession accessor = new DalSession(new SolonManagedDynamicConnection(ds));
+
+            varH.setValue(accessor);
+            return;
+        }
+
+        //@Db("db1") UserMapper ;
+        if (varH.getType().isInterface()) {
+            DalSession accessor = new DalSession(new SolonManagedDynamicConnection(ds), dalRegistry);
+
+            if (clz == BaseMapper.class) {
+                Object obj = accessor.createBaseMapper((Class<?>) varH.getGenericType().getActualTypeArguments()[0]);
+                varH.setValue(obj);
+            } else {
+                Object mapper = accessor.createMapper(varH.getType());
+                varH.setValue(mapper);
+            }
+            return;
+        }
+    }
+}
