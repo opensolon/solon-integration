9e8d2518a6124196b75d9041c257fbf69ed24d51
fuzi1996 <imfuzi@163.com>
2024-09-21 10:50:58 +0800

    ft:添加graphql订阅测试

--- solon-plugin/graphql-solon-plugin/src/test/java/features/test/MutationTest.java
+++ solon-plugin/graphql-solon-plugin/src/test/java/features/test/MutationTest.java
@@ -5,20 +5,14 @@ import static org.hamcrest.Matchers.is;
 
 import com.alibaba.fastjson.JSON;
 import demo.App;
-
+import demo.product.dto.ProductPriceHistoryDTO;
 import java.io.IOException;
 import java.util.Date;
 import java.util.LinkedHashMap;
-import java.util.concurrent.TimeUnit;
-
-import demo.product.dto.ProductPriceHistoryDTO;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.noear.java_websocket.client.SimpleWebSocketClient;
 import org.noear.snack.ONode;
 import org.noear.solon.core.util.ResourceUtil;
 import org.noear.solon.test.HttpTester;
-import org.noear.solon.test.SolonJUnit5Extension;
 import org.noear.solon.test.SolonTest;
 
 /**
@@ -27,7 +21,7 @@ import org.noear.solon.test.SolonTest;
  * @author lagnx
  */
 @SolonTest(value = App.class)
-public class MutationAndSubscriptionTest extends HttpTester {
+public class MutationTest extends HttpTester {
 
     @Test
     public void testAddMutation() throws IOException {
@@ -52,51 +46,7 @@ public class MutationAndSubscriptionTest extends HttpTester {
         assertThat(oNode.select("removeProduct.price").getInt(), is(product.getPrice()));
         assertThat(oNode.select("removeProduct.startDate").getString(), is(product.getStartDate().toString()));
     }
-
-    /**
-     * 订阅测试：未完成！会失败
-     */
-    /*@Test
-    public void testSubscription() throws IOException, InterruptedException {
-        ProductPriceHistoryDTO product = new ProductPriceHistoryDTO(123L, new Date(), 999);
-
-        ONode param = new ONode();
-        ONode variables = new ONode();
-        variables.set("productId", product.getId());
-        param.set("query",
-                ResourceUtil.getResourceAsString("/query/subscriptionProduct.gqls"));
-        param.set("variables", variables);
-        param.set("operationName", "testSubscription");
-        String json = param.toJson();
-
-        // socket
-        SimpleWebSocketClient client = new SimpleWebSocketClient("ws://127.0.0.1:8081/graphql") {
-            @Override
-            public void onMessage(String message) {
-                super.onMessage(message);
-                System.out.println("get a message: " + message);
-            }
-        };
-
-        client.connectBlocking(10, TimeUnit.SECONDS);
-        //定制心跳（可选）
-        //client.heartbeatHandler(new HeartbeatHandlerDefault());
-        //开始心跳 + 心跳时自动重连
-        client.heartbeat(20_000, true);
-
-        //发送测试
-        client.send(json);
-
-        //休息会儿
-        for(int i = 0; i < 100; i++) {
-            Thread.sleep(1000);
-            System.out.println("====");
-        }
-
-        //关闭（使用 release 会同时停止心跳及自动重连）
-        client.release();
-    }*/
-
+    
     private ONode addProduct(ProductPriceHistoryDTO product) throws IOException {
         ONode param = new ONode();
         ONode variables = new ONode();
