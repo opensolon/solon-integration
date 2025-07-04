e6598618e90b3fe8bc84b3c9f1a1b4f542802ce5
noear <noear@live.cn>
2025-07-04 11:43:59 +0800

    添加 hibernate-solon-plugin 对 PersistenceContext 注解的支持

--- solon-plugin-data-sql/hibernate-solon-plugin/src/main/java/org/hibernate/solon/integration/HibernateSolonPlugin.java
+++ solon-plugin-data-sql/hibernate-solon-plugin/src/main/java/org/hibernate/solon/integration/HibernateSolonPlugin.java
@@ -1,11 +1,15 @@
 package org.hibernate.solon.integration;
 
 import org.hibernate.solon.annotation.Db;
+import org.noear.solon.Utils;
 import org.noear.solon.core.AppContext;
 import org.noear.solon.core.Plugin;
+import org.noear.solon.core.VarHolder;
 import org.noear.solon.data.datasource.DsInjector;
+import org.noear.solon.data.datasource.DsUtils;
 
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import javax.persistence.spi.PersistenceProviderResolverHolder;
 import javax.sql.DataSource;
 
@@ -32,6 +36,31 @@ public class HibernateSolonPlugin implements Plugin {
 
         // 标准 jpa PersistenceContext 注入支持
         //PersistenceUnit
-        context.beanInjectorAdd(PersistenceContext.class, new PersistenceContextBeanInjector());
+        context.beanInjectorAdd(PersistenceContext.class, this::persistenceContextInject);
+        context.beanInjectorAdd(PersistenceUnit.class, this::persistenceUnitInject);
+    }
+
+    private void persistenceContextInject(VarHolder vh, PersistenceContext anno) {
+        String unitName = Utils.annoAlias(anno.unitName(), anno.name());
+
+        DsUtils.observeDs(vh.context(), unitName, dsBw -> {
+            HibernateAdapter adapter = HibernateAdapterManager.get(dsBw);
+
+            if (adapter != null) {
+                adapter.injectTo(vh);
+            }
+        });
+    }
+
+    private void persistenceUnitInject(VarHolder vh, PersistenceUnit anno) {
+        String unitName = Utils.annoAlias(anno.unitName(), anno.name());
+
+        DsUtils.observeDs(vh.context(), unitName, dsBw -> {
+            HibernateAdapter adapter = HibernateAdapterManager.get(dsBw);
+
+            if (adapter != null) {
+                adapter.injectTo(vh);
+            }
+        });
     }
 }
