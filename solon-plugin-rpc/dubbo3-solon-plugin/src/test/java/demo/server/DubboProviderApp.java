11a5be8d9e7f00efae119f6a13a5b0c5464e5b6d
noear <noear@live.cn>
2025-08-06 12:42:21 +0800

    3.4.3-SNAPSHOT

--- solon-plugin-rpc/dubbo3-solon-plugin/src/test/java/demo/server/DubboProviderApp.java
+++ solon-plugin-rpc/dubbo3-solon-plugin/src/test/java/demo/server/DubboProviderApp.java
@@ -1,11 +1,23 @@
 package demo.server;
 
+import demo.protocol.HelloService;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.noear.solon.Solon;
+import org.noear.solon.annotation.Managed;
 import org.noear.solon.extend.dubbo3.EnableDubbo;
 
 @EnableDubbo
+@Managed
 public class DubboProviderApp {
     public static void main(String[] args) throws InterruptedException{
         Solon.start(DubboProviderApp.class, args);
+
+        DubboProviderApp dubboProviderApp = Solon.app().context().getBean(DubboProviderApp.class);
+        String rst = dubboProviderApp.helloService.sayHello("world");
+
+        System.out.println(rst);
     }
+
+    @DubboReference(group = "demo")
+    HelloService helloService;
 }
